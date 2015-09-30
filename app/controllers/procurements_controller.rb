d2f2e1c0cc55a4cbadb955ade5dc757662dd52eb
madmatvey <potehin@gmail.com>
2015-09-30 23:39:20 +0400

    add auth limits for not singing in user

--- app/controllers/procurements_controller.rb
+++ app/controllers/procurements_controller.rb
@@ -1,6 +1,6 @@
 class ProcurementsController < ApplicationController
   before_action :set_procurement, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, :except => [:index]
+  before_action :authenticate_user!, :except => [:index,:show]
 
   # GET /procurements
   # GET /procurements.json
