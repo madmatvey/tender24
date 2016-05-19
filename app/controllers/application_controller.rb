6e2eefd24e6c5f20debde4ed09c4322029da30ff
madmatvey <potehin@gmail.com>
2016-05-19 12:28:22 +0400

    active admin+devise+cancan initialize ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
-  def authenticate_active_admin_user!
+
+  def authenticate_admin_user!
    authenticate_user!
    unless current_user.role?(:admin)
       flash[:alert] = "You are not authorized to access this resource!"
       redirect_to root_path
    end
  end
+
 end
--- app/controllers/procurements_controller.rb
+++ app/controllers/procurements_controller.rb
@@ -1,9 +1,9 @@
 class ProcurementsController < InheritedResources::Base
-
+  load_and_authorize_resource
+  
   private
 
     def procurement_params
       params.require(:procurement).permit(:name, :details)
     end
 end
-
